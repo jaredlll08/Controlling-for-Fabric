76013effdd9c78d5bfdc5fe64da2bf8013cb3c57
Pyrofab <redstoneinfire@gmail.com>
2019-08-05 23:34:06 +0200

    Fix refmap generation

--- src/main/java/com/blamejared/fabriccontrolling/mixin/OpenGuiMixin.java
+++ src/main/java/com/blamejared/fabriccontrolling/mixin/OpenGuiMixin.java
@@ -7,13 +7,20 @@ import net.minecraft.client.gui.menu.options.ControlsOptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class OpenGuiMixin {
     
     @Shadow
     public Screen currentScreen;
+
+    @Inject(method = "openScreen", at = @At("HEAD"))
+    private void dummyGenerateRefmap(Screen screen, CallbackInfo ci) {
+        // NO-OP this injection is only here to generate the refmap
+    }
 
     @ModifyVariable(method = "openScreen", at = @At("HEAD"), argsOnly = true)
     private Screen upgradeControlScreen(Screen opened) {
